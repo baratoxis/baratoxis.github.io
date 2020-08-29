7aa30e83b0d466be195a506771cb21a6f762252c
baratoxis <33399712+baratoxis@users.noreply.github.com>
2020-08-29 16:19:40 +0100

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,129 @@
+﻿/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.04
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "Fallout Awakening";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*Video background
+Avaiable options:
+"backgrounds/videos/aurora.webm"
+"backgrounds/videos/bluefall.webm"
+"backgrounds/videos/bluetilefloor.webm"
+"backgrounds/videos/bluewhitecircles.webm"
+"backgrounds/videos/bokehlens.webm"
+"backgrounds/videos/cleanbokeh.webm"
+"backgrounds/videos/coldnight.webm"
+"backgrounds/videos/colorfulburst.webm"
+"backgrounds/videos/dancingstars.webm"
+"backgrounds/videos/dropsflares.webm"
+"backgrounds/videos/focusorangebokeh.webm"
+"backgrounds/videos/glimmering.webm"
+"backgrounds/videos/glowingrings.webm"
+"backgrounds/videos/greencircles.webm"
+"backgrounds/videos/hues.webm"
+"backgrounds/videos/inthedeep.webm"
+"backgrounds/videos/modernflourish.webm"
+"backgrounds/videos/orangeaura.webm"
+"backgrounds/videos/warminside.webm"
+"backgrounds/videos/yellowgreen.webm"
+
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "";
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/fallout.jpg",
+	"backgrounds/images/fallout01.jpg",
+	"backgrounds/images/fallout02.jpg",
+	"backgrounds/images/fallout03.jpg",
+    "backgrounds/images/fallout04.jpg",
+	"backgrounds/images/fallout05.jpg"
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+        {youtube: "KXSUEU7ISfQ", name: "Five Stars - Atom Bomb Baby"},
+	{youtube: "fEzqndjwwIA", name: "Dion - The Wanderer"},
+        {youtube: "QSBco8kVuZM", name: "Way Back Home"},
+        {youtube: "zndUfV1i1Co", name: "Civilization"},
+        {youtube: "zzICMIu5zFY", name: "Big Iron"}           
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Awaken from your nightmare, my child!",
+"Any similarity to actual persons living or dead is purely coincidental.",
+"Join our discord https://discord.gg/GARYTgp!",
+"Imagine paying for an Outcast faction LOL",
+"Quests can be found across the map. They contain various rewards.",
+"Remember to stay hydrated and sated, since it brings out buffs!",
+"Let me pay for caps, VIP and faction whitelists, oh wait this isn't the 'normal' FalloutRP!"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
